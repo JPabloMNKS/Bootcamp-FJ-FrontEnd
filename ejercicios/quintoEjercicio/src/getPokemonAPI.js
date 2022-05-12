956abf2aa640d2d8a56655f672abd9df8564ebf7
Juan Pablo Bedoya <jpablo.bedoya7@gmail.com>
2022-05-12 11:37:15 -0400

    less css implemented

---
+++ ejercicios/quintoEjercicio/src/getPokemonAPI.js
@@ -0,0 +1,17 @@
+fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=45")
+.then((response) => response.json())
+.then((pokemons) => {
+  pokemons.results.forEach((pokemon, index) => {
+    const card = document.createElement("poke-card");
+    const backgroundColor = pokemonColorMap[index + 1];
+    const color = backgroundColor[1] === "f" ? "#000" : "#fff";
+    card.setAttribute("id", index + 1);
+    card.setAttribute("name", pokemon.name);
+    card.setAttribute("image", getPokemonImageUri(index + 1));
+    card.setAttribute("backgroundColor", backgroundColor);
+    card.setAttribute("text-color", color);
+    document.getElementById("poke_container").appendChild(card);
+  });
+
+  customElements.define("poke-card", PokemonCard);
+});
---
+++ ejercicios/quintoEjercicio/src/pokemonCard.js
@@ -0,0 +1,19 @@
+class PokemonCard extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadow = this.attachShadow({ mode: "closed" });
+      const templateElem = document.getElementById("pokemonCardTemplate");
+      const content = templateElem.content.cloneNode(true);
+      content
+        .querySelector("img.image")
+        .setAttribute("src", this.getAttribute("image"));
+      content.querySelector(".card").style.backgroundColor =
+        this.getAttribute("backgroundColor");
+      const name = content.querySelector(".card>.name");
+      name.innerText = this.getAttribute("name");
+      name.style.color = this.getAttribute("text-color");
+
+      shadow.appendChild(content);
+    }
+  }
