b4794b3f4f7bdb99f029abcbfdc4fa4370a97d14
Juan Pablo Bedoya <jpablo.bedoya7@gmail.com>
2022-05-20 12:53:03 -0400

    pokemon grunt gulp challenge

--- ejercicios/quintoEjercicio/Gruntfile.js
+++ ejercicios/quintoEjercicio/Gruntfile.js
@@ -1,15 +1,49 @@
-module.exports = function(grunt){
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: ['dist'],
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: ['src/*.js'],
+                dest: 'dist/<%= pkg.name %>.js',
+            },
+        },
+        less: {
+            build: {
+                files: {
+                    'dist/style/<%= pkg.name %>.css': 'src/*.less',
+                },
+            },
+        },
+        htmlbuild: {
+            dist: {
+                src: 'src/*.html',
+                dest: 'dist/html',
+                options: {
+                    scripts: { main: 'dist/<%= pkg.name %>.min.js' },
+                },
+            },
+        },
         uglify: {
             build: {
                 files: {
-                    'dist/js/build.min.js':'src/*.js',
-                    
+                    'dist/js/<%= pkg.name %>.min.js': 'src/*.js',
+
                 }
             }
-        }
+        },
+        watch: {
+            tasks: ['concat', 'uglify', 'less'],
+        },
     });
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default',['uglify']);
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-html-build');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['clean', 'concat', 'uglify', 'less', 'htmlbuild']);
 }
