9ac52c6032cc2c0b997d46cebd8f5223cd89f009
Juan Pablo Bedoya <jpablo.bedoya7@gmail.com>
2022-06-06 16:11:11 -0400

    Decoradores con pokemon challenge

--- ejercicios/decoradores/decoradores.ts
+++ ejercicios/decoradores/decoradores.ts
@@ -1,7 +1,26 @@
 type Move = {
-    name: string,
-    power: number
+  name: string,
+  power: number
 };
+
+const checkPP = (ppAvailable: number) => (
+  target: any,
+  prop: string,
+  descriptor: PropertyDescriptor
+) => {
+  const original = descriptor.value;
+
+  descriptor.value = function (...args: any) {
+    if (this.ppAvailable > ppAvailable)
+      original.apply(this, args);
+    else
+      console.log('Not enough PP');
+
+  }
+  return descriptor
+}
+
+
 
 class Pokemon {
   name: string;
@@ -12,10 +31,10 @@ class Pokemon {
   }
 
 
-  @checkPP()
+  @checkPP(1)
   figth(move: Move) {
     console.log(`${this.name} used ${move?.name}!`);
-    this.ppAvailable -= 1;
+    this.ppAvailable = this.ppAvailable - 1;
   }
 
   calculateDamage(move: any) {
@@ -23,12 +42,9 @@ class Pokemon {
   }
 }
 
-const thunderbolt: Move = {name: 'thunderbolt', power: 90};
-const pikachu = new Pokemon('pikachu', 1);
+const thunderbolt: Move = { name: 'thunderbolt', power: 90 };
+const pikachu = new Pokemon('pikachu', 2);
 pikachu.figth(thunderbolt);
 pikachu.figth(thunderbolt);
 
-function checkPP() {
-  
-throw new Error("Function not implemented.");
-}
+
