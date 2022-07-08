7c6915712f4de08c9b156d8654843c6df95083c1
Juan Pablo Bedoya <jpablo.bedoya7@gmail.com>
2022-07-08 10:19:12 -0400

    changes in the profile

--- pokeApi/src/app/pokemons/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ pokeApi/src/app/pokemons/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -2,28 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { PokemonService } from '../../../services/pokemon.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from 'src/app/core/interfaces/pokemon.interface';
+import { pokemonColorMap } from 'src/app/utils/utils';
 
 @Component({
   selector: 'pokemon-profile-component',
   templateUrl: './pokemon-profile.component.html',
+  styleUrls: ['./pokemon-profile.component.scss'],
 })
 export class PokemonProfileComponent implements OnInit {
   id: string = '1';
-  fields: any;
+  fields!: any;
+  pokemon!: Pokemon;
 
   constructor(
     private location: Location,
     private pokemonService: PokemonService,
     private route: ActivatedRoute
   ) {}
+
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') || '1';
-    this.pokemonService.getPokemon(this.id).subscribe((pokemonDetail) => {
-      this.fields = pokemonDetail;
+    this.pokemonService.getPokemon(this.id).subscribe((pokemon) => {
+      this.fields = pokemon.results;
     });
   }
 
   goBack(): void {
     this.location.back;
   }
+
+
+
+
+
 }
