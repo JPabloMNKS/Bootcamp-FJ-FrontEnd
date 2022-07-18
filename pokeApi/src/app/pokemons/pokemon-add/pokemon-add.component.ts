7eb515b01b4d0711f78e252bbea3ddc2077eb24a
Juan Pablo Bedoya <jpablo.bedoya7@gmail.com>
2022-07-18 17:28:05 -0400

    Small changes in the validators

--- pokeApi/src/app/pokemons/pokemon-add/pokemon-add.component.ts
+++ pokeApi/src/app/pokemons/pokemon-add/pokemon-add.component.ts
@@ -1,39 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { PokemonDescription } from '../../core/interfaces/pokemon.interface';
 
 @Component({
   selector: 'app-pokemon-add',
   templateUrl: './pokemon-add.component.html',
-  styleUrls: ['./pokemon-add.component.scss']
+  styleUrls: ['./pokemon-add.component.scss'],
 })
 export class PokemonAddComponent implements OnInit {
-
   profileForm = new FormGroup({
-    pokemonName: new FormControl(''),
-    pokemonDescription: new FormControl(''),
-    pokemonHeight: new FormControl(''),
-    pokemonWeight: new FormControl(''),
-    pokemonType: new FormControl(''),
+    pokemonName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(25),
+    ]),
+    pokemonDescription: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(75),
+    ]),
+    pokemonHeight: new FormControl('', [
+      Validators.required
+    ]),
+    pokemonWeight: new FormControl('', [
+      Validators.required,
+    ]),
+    pokemonType: new FormControl('', [
+      Validators.required,
+    ]),
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
-  typesOfPokemon = [ 'Normal', 'Fighting', 'Flying', 'Poison', 'Fire', 'Water'];
+  typesOfPokemon = ['Normal', 'Fighting', 'Flying', 'Poison', 'Fire', 'Water'];
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  get types(){
+  get types() {
     return this.profileForm.get('types') as FormArray;
   }
 
-  addType(){
+  addType() {
     this.types.push(this.fb.control(''));
   }
 
-  onSubmit(){
+  onSubmit() {
     console.warn(this.profileForm.value);
   }
-
 }
